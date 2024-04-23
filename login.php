ece763ff7b96e0856c93973ac4234b6d09499b7e
ZakariaAyad1 <zakariaayad27@gmail.com>
2024-04-23 19:57:19 +0100

    modify the icons

--- login.php
+++ login.php
@@ -32,38 +32,38 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>home</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
 
-<?php include 'components/user_header.php'; ?>
+    <?php include 'components/user_header.php'; ?>
 
-<section class="form-container">
+    <section class="form-container">
 
-   <form action="" method="post" enctype="multipart/form-data" class="login">
-      <h3>welcome back!</h3>
-      <p>your email <span>*</span></p>
-      <input type="email" name="email" placeholder="enter your email" maxlength="50" required class="box">
-      <p>your password <span>*</span></p>
-      <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
-      <p class="link">don't have an account? <a href="register.php">register now</a></p>
-      <input type="submit" name="submit" value="login now" class="btn">
-   </form>
+        <form action="" method="post" enctype="multipart/form-data" class="login">
+            <h3>welcome back!</h3>
+            <p>your email <span>*</span></p>
+            <input type="email" name="email" placeholder="enter your email" maxlength="50" required class="box">
+            <p>your password <span>*</span></p>
+            <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
+            <p class="link">don't have an account? <a href="register.php">register now</a></p>
+            <input type="submit" name="submit" value="login now" class="btn">
+        </form>
 
-</section>
-
-
+    </section>
 
 
 
@@ -75,8 +75,11 @@ if(isset($_POST['submit'])){
 
 
 
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-   
+
+
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
+
 </body>
+
 </html>
