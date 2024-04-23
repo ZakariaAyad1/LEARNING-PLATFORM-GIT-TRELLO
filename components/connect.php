e46fea85c0ba159aaa48fb8e065bb9ed1733b042
ZakariaAyad1 <zakariaayad27@gmail.com>
2024-04-23 14:14:46 +0100

    modify the three icons of the header

--- components/connect.php
+++ components/connect.php
@@ -1,18 +1,34 @@
 <?php
-
-   $db_name = 'mysql:host=localhost:3307;dbname=course_db';
+   $db_host = 'localhost';
+   $db_port = '3308'; // Port du serveur de base de données
+   $db_name = 'apprentissage';
    $user_name = 'root';
-   $user_password = 'hibachioua@';
+   $user_password = '';
 
-   $conn = new PDO($db_name, $user_name, $user_password);
+   $dsn = "mysql:host=$db_host;port=$db_port;dbname=$db_name";
 
-   function unique_id() {
+   try {
+      // Connexion à la base de données
+      $conn = new PDO($dsn, $user_name, $user_password);
+      // Définir le mode d'erreur PDO sur exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      // Autres configurations PDO si nécessaire
+   } catch (PDOException $e) {
+      // En cas d'erreur de connexion, afficher le message d'erreur
+      echo "Erreur de connexion à la base de données : " . $e->getMessage();
+      // Arrêter le script ou gérer l'erreur selon votre besoin
+      exit();
+   }
+
+   // Définition de la fonction unique_id() si nécessaire
+   function unique_id()
+   {
       $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
       $rand = array();
       $length = strlen($str) - 1;
       for ($i = 0; $i < 20; $i++) {
-          $n = mt_rand(0, $length);
-          $rand[] = $str[$n];
+         $n = mt_rand(0, $length);
+         $rand[] = $str[$n];
       }
       return implode($rand);
    }
