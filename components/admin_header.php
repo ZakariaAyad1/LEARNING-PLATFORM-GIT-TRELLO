fa3a1ccdd56f563ce61b9c3a55cce606c058f593
Hibachioua <93033112+Hibachioua@users.noreply.github.com>
2024-05-02 12:12:09 +0100

    delete search bar from admin

--- components/admin_header.php
+++ components/admin_header.php
@@ -17,14 +17,10 @@ if(isset($message)){
 
       <a href="dashboard.php" class="logo">Admin</a>
 
-      <form action="search_page.php" method="post" class="search-form">
-         <input type="text" name="search" placeholder="search here..." required maxlength="100">
-         <button type="submit" class="fas fa-search" name="search_btn"></button>
-      </form>
+      
 
       <div class="icons">
          <div id="menu-btn" class="fas fa-bars"></div>
-         <div id="search-btn" class="fas fa-search"></div>
          <div id="user-btn" class="fas fa-user"></div>
          
       </div>
